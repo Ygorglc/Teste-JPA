0faec4eb5b908ca1001aa69954b8feec46f578ab
Ygor <ygorglc@gmail.com>
2021-10-27 14:04:49 -0300

    Atualizações

---
+++ src/main/java/com/demo/DemoApiApplication/jpa/CadastroCozinha.java
@@ -0,0 +1,29 @@
+package com.demo.DemoApiApplication.jpa;
+
+import com.demo.DemoApiApplication.domain.model.Cozinha;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.Entity;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+@Component
+public class CadastroCozinha {
+
+    @PersistenceContext
+    private EntityManager manager;
+
+    public List<Cozinha> listar() {
+        return manager.createQuery("from Cozinha", Cozinha.class)
+                .getResultList();
+    }
+
+    @Transactional
+    public Cozinha adicionar(Cozinha cozinha){
+         return manager.merge(cozinha);
+    }
+
+}
---
+++ src/main/java/com/demo/DemoApiApplication/jpa/InclusaoCozinhaMain.java
@@ -0,0 +1,32 @@
+package com.demo.DemoApiApplication.jpa;
+
+import com.demo.DemoApiApplication.DemoApiApplication;
+import com.demo.DemoApiApplication.domain.model.Cozinha;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+
+public class InclusaoCozinhaMain {
+
+    public static void main(String[] args){
+
+        ConfigurableApplicationContext applicationContext = new SpringApplicationBuilder(DemoApiApplication.class)
+                .web(WebApplicationType.NONE)
+                .run(args);
+
+        CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
+
+        Cozinha cozinha1 = new Cozinha();
+        cozinha1.setNome("Brasil");
+
+        Cozinha cozinha2 = new Cozinha();
+        cozinha2.setNome("Japonesa");
+
+        cadastroCozinha.adicionar(cozinha1);
+        cadastroCozinha.adicionar(cozinha2);
+
+        System.out.printf("%d - %s\n", cozinha1.getId(), cozinha1.getNome());
+        System.out.printf("%d - %s\n", cozinha2.getId(), cozinha2.getNome());
+    }
+
+}
